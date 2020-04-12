8bc98ff1a270a0324ee4f262a0597c8d19b0c64d
Rafal Jakimowicz <bobix7913@gmail.com>
2020-04-12 20:42:29 +0200

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txbPath = new System.Windows.Forms.TextBox();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.txbPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbFileOrFolder = new System.Windows.Forms.RadioButton();
+            this.rbDisc = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnEncrypt.Location = new System.Drawing.Point(517, 245);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(132, 48);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDecrypt.Location = new System.Drawing.Point(9, 245);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(122, 48);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txbPath
+            // 
+            this.txbPath.Enabled = false;
+            this.txbPath.Location = new System.Drawing.Point(9, 13);
+            this.txbPath.Name = "txbPath";
+            this.txbPath.Size = new System.Drawing.Size(594, 20);
+            this.txbPath.TabIndex = 2;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(609, 11);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(40, 23);
+            this.btnChoose.TabIndex = 3;
+            this.btnChoose.Text = "...";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // txbPass
+            // 
+            this.txbPass.Location = new System.Drawing.Point(187, 176);
+            this.txbPass.Name = "txbPass";
+            this.txbPass.Size = new System.Drawing.Size(340, 20);
+            this.txbPass.TabIndex = 4;
+            this.txbPass.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(100, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Password";
+            // 
+            // rbFileOrFolder
+            // 
+            this.rbFileOrFolder.AutoSize = true;
+            this.rbFileOrFolder.Location = new System.Drawing.Point(212, 104);
+            this.rbFileOrFolder.Name = "rbFileOrFolder";
+            this.rbFileOrFolder.Size = new System.Drawing.Size(85, 17);
+            this.rbFileOrFolder.TabIndex = 6;
+            this.rbFileOrFolder.TabStop = true;
+            this.rbFileOrFolder.Text = "File or Folder";
+            this.rbFileOrFolder.UseVisualStyleBackColor = true;
+            // 
+            // rbDisc
+            // 
+            this.rbDisc.AutoSize = true;
+            this.rbDisc.Location = new System.Drawing.Point(421, 104);
+            this.rbDisc.Name = "rbDisc";
+            this.rbDisc.Size = new System.Drawing.Size(46, 17);
+            this.rbDisc.TabIndex = 7;
+            this.rbDisc.TabStop = true;
+            this.rbDisc.Text = "Disc";
+            this.rbDisc.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(137, 245);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(374, 48);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 299);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.rbDisc);
+            this.Controls.Add(this.rbFileOrFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbPass);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.txbPath);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txbPath;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.TextBox txbPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbFileOrFolder;
+        private System.Windows.Forms.RadioButton rbDisc;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,374 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string FilePath = "";
+        string Password = "";
+        List<string> files = new List<string>();
+        string SALT = "*sHa256";
+
+        public Form1()
+        {
+            InitializeComponent();
+            rbFileOrFolder.Checked = true;
+        }
+
+        //  Call this function to remove the key from memory after use for security
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destination, int Length);
+
+        /// <summary>
+        /// Creates a random salt that will be used to encrypt your file. This method is required on FileEncrypt.
+        /// </summary>
+        /// <returns></returns>
+        public static byte[] GenerateRandomSalt()
+        {
+            byte[] data = new byte[32];
+
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Fille the buffer with the generated data
+                    rng.GetBytes(data);
+                }
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Encrypts a file from its path and a plain password.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="password"></param>
+        private void FileEncrypt(string inputFile, string password)
+        {
+
+            //generate random salt
+            byte[] salt = GenerateRandomSalt();
+
+            //create output file name
+            FileStream fsCrypt = new FileStream(inputFile + ".mvp", FileMode.Create);
+
+            //convert password string to byte arrray
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(GenaratePassword(password));
+
+            //Set Rijndael symmetric encryption algorithm
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            AES.Padding = PaddingMode.PKCS7;
+
+
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+
+            AES.Mode = CipherMode.CFB;
+
+            // write salt to the begining of the output file, so in this case can be random every time
+            fsCrypt.Write(salt, 0, salt.Length);
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write);
+
+            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+            //create a buffer (10mb) so only this amount will allocate in the memory and not the whole file
+            byte[] buffer = new byte[536870912];
+            int read;
+
+            try
+            {
+                while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents(); // -> for responsive GUI, using Task will be better!
+                    cs.Write(buffer, 0, read);
+                }
+
+                // Close up
+                fsIn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                cs.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        /// <summary>
+        /// Decrypts an encrypted file with the FileEncrypt method through its path and the plain password.
+        /// </summary>
+        /// <param name="inputFile"></param>
+        /// <param name="outputFile"></param>
+        /// <param name="password"></param>
+        private void FileDecrypt(string inputFile, string outputFile, string password)
+        {
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(GenaratePassword(password));
+            byte[] salt = new byte[32];
+
+            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+            fsCrypt.Read(salt, 0, salt.Length);
+
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;
+            AES.BlockSize = 128;
+            var key = new Rfc2898DeriveBytes(passwordBytes, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Padding = PaddingMode.PKCS7;
+            AES.Mode = CipherMode.CFB;
+
+            CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read);
+
+            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+            int read;
+            //10mb buffer
+            byte[] buffer = new byte[536870912];
+
+            try
+            {
+                while ((read = cs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    Application.DoEvents();
+                    fsOut.Write(buffer, 0, read);
+                }
+            }
+            catch (CryptographicException ex_CryptographicException)
+            {
+                Console.WriteLine("CryptographicException error: " + ex_CryptographicException.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            try
+            {
+                cs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error by closing CryptoStream: " + ex.Message);
+            }
+            finally
+            {
+                fsOut.Close();
+                fsCrypt.Close();
+            }
+        }
+
+        private string GenaratePassword(string raw)
+        {
+            using (SHA512 sha = SHA512.Create())
+            {
+                byte[] bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(raw + SALT));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }       
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            FilePath = ((string[])e.Data.GetData(DataFormats.FileDrop, false))[0];
+            txbPath.Text = FilePath;
+        }
+
+        private string Ziping(string path)
+        {
+            using (ZipFile zip = new ZipFile())
+            {
+                zip.AddDirectory(path, "Files");
+                zip.CompressionLevel = Ionic.Zlib.CompressionLevel.Level0;
+                zip.Save(path + ".zip");
+                return path + ".zip";
+            }
+        }
+
+        private void Unziping(string path)
+        {
+            if (new FileInfo(path).Extension == ".zip")
+            {
+                using (ZipFile zip = ZipFile.Read(path))
+                {
+                    string output = path.Substring(0, path.Length - 4);
+                    Directory.CreateDirectory(output);
+                    foreach (ZipEntry item in zip)
+                    {
+                        item.Extract(output, ExtractExistingFileAction.OverwriteSilently);
+                    }
+                }
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            try
+            {
+                if (rbFileOrFolder.Checked)
+                {
+                    progressBar1.Maximum = 1;
+                    FileAttributes attr = File.GetAttributes(FilePath);
+                    if (attr.HasFlag(FileAttributes.Directory))
+                    {
+                        string zipfile = Ziping(FilePath);
+                        FileEncrypt(zipfile, Password);
+                        File.Delete(zipfile);
+                    }
+                    else
+                    {
+                        FileEncrypt(FilePath, Password);
+                    }
+                    progressBar1.Value += 1;
+                }
+                else if (rbDisc.Checked)
+                {
+                    progressBar1.Maximum = files.Count;
+                    foreach (var item in files)
+                    {
+                        FileAttributes attr = File.GetAttributes(item);
+                        if (attr.HasFlag(FileAttributes.Directory))
+                        {
+                            string zipfile = Ziping(item);
+                            FileEncrypt(zipfile, Password);
+                            File.Delete(zipfile);
+                        }
+                        else
+                        {
+                            FileEncrypt(item, Password);
+                        }
+                        progressBar1.Value += 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Password = txbPass.Text;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            try
+            {
+                if (rbFileOrFolder.Checked)
+                {
+                    progressBar1.Maximum = 1;
+                    string output = FilePath.Substring(0, FilePath.Length - 4);
+                    FileDecrypt(FilePath, output, Password);
+                    try
+                    {
+                        Unziping(output);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    progressBar1.Value += 1;
+                }
+                else if (rbDisc.Checked)
+                {
+                    progressBar1.Maximum = files.Count;
+                    foreach (var item in files)
+                    {
+                        string output = item.Substring(0, item.Length - 4);
+                        FileDecrypt(item, output, Password);
+                        try
+                        {
+                            Unziping(output);
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                        progressBar1.Value += 1;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            if (rbFileOrFolder.Checked)
+            {
+                using (OpenFileDialog opf = new OpenFileDialog())
+                {
+                    if (opf.ShowDialog() == DialogResult.OK)
+                    {
+                        FilePath = opf.FileName;
+                    }
+                }
+            }
+            else if (rbDisc.Checked)
+            {
+                using (var fbd = new FolderBrowserDialog())
+                {
+                    DialogResult result = fbd.ShowDialog();
+
+                    if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                    {
+                        string[] file = Directory.GetFiles(fbd.SelectedPath);
+                        string[] dirs = Directory.GetDirectories(fbd.SelectedPath);
+                        for (int i = 1; i < dirs.Length; i++)
+                        {
+                            files.Add(dirs[i]);
+                        }
+                        foreach (var item in file)
+                        {
+                            files.Add(item);
+                        }
+                        txbPath.Text = fbd.SelectedPath;
+                    }
+                }
+            }
+        }
+    }
+}
