a9965929ec78fc41f62dabe07040a8a44d3eb2b4
Rafal Jakimowicz <bobix7913@gmail.com>
2020-04-13 20:07:33 +0200

    master

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -89,7 +89,7 @@
             // 
             // txbPass
             // 
-            this.txbPass.Location = new System.Drawing.Point(120, 153);
+            this.txbPass.Location = new System.Drawing.Point(121, 187);
             this.txbPass.Name = "txbPass";
             this.txbPass.Size = new System.Drawing.Size(222, 20);
             this.txbPass.TabIndex = 4;
@@ -99,7 +99,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(11, 153);
+            this.label1.Location = new System.Drawing.Point(37, 187);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 5;
