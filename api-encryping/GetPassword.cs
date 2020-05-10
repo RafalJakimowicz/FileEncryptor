adcc9a0eb6c2bd8e8f2d73eea6d8d4ef7fa7b43b
RafalJakimowicz <bobix7913@gmail.com>
2020-05-10 23:36:03 +0200

    master

--- api-encryping/GetPassword.cs
+++ api-encryping/GetPassword.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,10 +9,9 @@ namespace api_encryping.secure.password
 {
     static class GetPassword
     {
-        private static byte[] passbytes = Encoding.UTF8.GetBytes("%4$^7api_encryping.secure.password");
         public static string getPass()
         {
-            return BitConverter.ToString(passbytes);
+            return "%4$^7api_encryping.secure.password";
         }
     }
 }
