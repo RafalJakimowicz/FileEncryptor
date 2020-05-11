85224a26b07716d5badab5aed9f06523f30d6417
RafalJakimowicz <bobix7913@gmail.com>
2020-05-11 18:46:00 +0200

    master

--- api-securing/SecureFile.cs
+++ api-securing/SecureFile.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using api_encryping.aes;
 using api_encryping.jsons;
 
@@ -13,6 +17,7 @@ namespace api_encryping.secure
     {
         private FILEPATH paths;
         public FILEPATH Paths { get => paths; set => paths = value; }
+        private string password;
 
         JsonsToPaths jtp;
         private static string FOLDER = "./cache/secured";
@@ -32,8 +37,29 @@ namespace api_encryping.secure
             List<FILEPATH> lf = jtp.Deserialize();
             foreach (var item in lf)
             {
-
+                if(item.PrevFile == _path)
+                {
+                    MessageBox.Show("That file is already secured");
+                    return;
+                }
             }
+            byte[] fileBytes = File.ReadAllBytes(_path);
+            SHA1 hash = new SHA1CryptoServiceProvider();
+            byte[] hashBytes = hash.ComputeHash(fileBytes);
+            string FileName = BitConverter.ToString(hashBytes);
+            FileInfo fi = new FileInfo(_path);
+            string newFileFullPath = FOLDER + "/" + FileName + fi.Extension;
+            FILEPATH fp = new FILEPATH() { NewFile = newFileFullPath, PrevFile = _path };
+            lf.Add(fp);
+            Paths = fp;
+            password = FileName;
+            Thread t = new Thread(StartEncrypting);
+            t.Start();
+        }
+
+        private void StartEncrypting()
+        {
+            a.FileEncrypt(Paths.PrevFile, password, Paths.NewFile);
         }
 
         public void GetAccess(string _path)
