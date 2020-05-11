14bce0d2ed2d564ba06c867cfd043460cc6ec619
RafalJakimowicz <bobix7913@gmail.com>
2020-05-11 18:19:44 +0200

    master

---
+++ api-securing/SecureFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using api_encryping.aes;
+using api_encryping.jsons;
+
+namespace api_encryping.secure
+{
+    public class SecureFile
+    {
+        private FILEPATH paths;
+        public FILEPATH Paths { get => paths; set => paths = value; }
+
+        JsonsToPaths jtp;
+        private static string FOLDER = "./cache/secured";
+        AESCrypting a;
+        public SecureFile()
+        {
+            jtp = new JsonsToPaths();
+            a = new AESCrypting(true);
+            if (Directory.Exists(FOLDER))
+            {
+                Directory.CreateDirectory(FOLDER);
+            }
+        }
+
+        public void AddToSecure(string _path)
+        {
+            List<FILEPATH> lf = jtp.Deserialize();
+            foreach (var item in lf)
+            {
+
+            }
+        }
+
+        public void GetAccess(string _path)
+        {
+            
+        }
+    }
+}
---
+++ api-securing/SecureFolder.cs
@@ -0,0 +1,38 @@
+﻿using api_encryping.aes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_encryping.secure
+{
+    public class SecureFolder
+    {
+        private string path;
+
+        public string Path { get => path; set => path = value; }
+
+        private static string FOLDER = "./cache/secured";
+        AESCrypting a;
+        public SecureFolder()
+        {
+            a = new AESCrypting(true);
+            if (Directory.Exists(FOLDER))
+            {
+                Directory.CreateDirectory(FOLDER);
+            }
+        }
+
+        public void AddToSecure(string _path)
+        {
+
+        }
+
+        public void GetAccess(string _path)
+        {
+
+        }
+    }
+}
