4a1b8adfeaa75915e87ccb0dfe831504e64949d1
RafalJakimowicz <bobix7913@gmail.com>
2020-05-11 12:58:21 +0200

    master

--- api-encryping/JsonsToPaths.cs
+++ api-encryping/JsonsToPaths.cs
@@ -8,19 +8,30 @@ using Newtonsoft.Json;
 
 namespace api_encryping.jsons
 {
+    public class FILEPATH 
+    { 
+        public string PrevFile { get; set; }
+        public string NewFile { get; set; }
+    }
     public class JsonsToPaths
     {
         private class RootObject
         {
-            private 
+            public List<FILEPATH> pathes { get; set; }
         }
-        private const string PATH_TO_JSON = "./cache/secured/jsons";
+        private const string PATH_TO_JSON = "./cache/secured/jsons/pathes.json";
         public JsonsToPaths()
         {
             if (!File.Exists(PATH_TO_JSON))
             {
 
             }
+        }
+        public void Serialize(FILEPATH _pathes)
+        {
+            RootObject ro = new RootObject() { pathes = new List<FILEPATH> { _pathes } };
+            string json = JsonConvert.SerializeObject(ro, Formatting.Indented);
+            File.WriteAllText(PATH_TO_JSON, json);
         }
     }
 }
